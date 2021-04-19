a164ef0b875a7c37b1e6ceb34329e8a2a7a47256
mineshine11 <d40614120@go.meiho.edu.tw>
2021-04-20 01:23:45 +0800

    Update bot.py

--- bot.py
+++ bot.py
@@ -8,4 +8,14 @@ bot=commands.Bot(command_prefix='[')
 async def on_ready():
     print(">> Bot is online <<")
 
-#bot.run("ODMzNzMwODU4MTM2ODk1NTI4.YH2mXg.tr-mKemjO8OKnAx673b970qTX40")
+@bot.event
+async def on_member_join(member):
+    channel = bot.get_channel(833753121288355922)
+    await channel.send(f'{member} join!')
+
+@bot.event
+async def on_member_remove(member):
+    channel = bot.get_channel(833753233490706462)
+    await channel.send(f'{member} leave!')
+
+bot.run("ODMzNzMwODU4MTM2ODk1NTI4.YH2mXg.tr-mKemjO8OKnAx673b970qTX40")
